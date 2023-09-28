1f03fedc10f9b66ed2eb96e3db74570171e4ea1f
Enrico Weigelt, metux IT consult <info@metux.net>
2023-09-28 16:27:49 +0200

    api: add safe frontend functions
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

---
+++ api/frontend.go
@@ -0,0 +1,83 @@
+package api
+
+import (
+    "strings"
+    "strconv"
+)
+
+// get a single string value from dict and key
+func GetStr(r Entry, k Key) string {
+    if r == nil {
+        return ""
+    }
+    if e, err := r.Get(k); e == nil || err != nil {
+        return ""
+    } else {
+        return e.String()
+    }
+}
+
+// append the string value(s) of entry to given slice
+// if entry is list or dict, collect the string values of its members
+// (also done recursively)
+func StrListAppendEntry(s[] string, e Entry) []string {
+    if e == nil {
+        return s
+    }
+    if e.IsScalar() {
+        return append(s, e.String())
+    }
+    for _,v := range e.Elems() {
+        s = StrListAppendEntry(s, v)
+    }
+    return s
+}
+
+// fetch entry from dict by key and return aa slice of string values
+// if an entry is list or dict, collect the string values of its members
+// (also done recursively)
+func GetStrList(r Entry, k Key) []string {
+    s := []string{}
+    if r == nil {
+        return s
+    }
+    v, err := r.Get(k);
+    if v == nil || err != nil {
+        return s
+    }
+    return StrListAppendEntry(s, v)
+}
+
+func GetBool(r Entry, k Key, dflt bool) bool {
+    switch strings.ToLower(GetStr(r, k)) {
+        case "1", "y", "yes", "true",  "on":  return true
+        case "0", "n", "no",  "false", "off": return false 
+        default:                              return dflt
+    }
+}
+
+func GetInt(r Entry, k Key, dflt int) int {
+    if i, err := strconv.Atoi(GetStr(r, k)); err == nil {
+        return i
+    } else {
+        return dflt
+    }
+}
+
+func GetKeys(r Entry, k Key) []string {
+    if r != nil {
+        if ent,err := r.Get(k); err == nil || ent != nil {
+            return ent.Keys()
+        }
+    }
+    return []string{}
+}
+
+func GetElems(r Entry, k Key) []Entry {
+    if r != nil {
+        if ent,err := r.Get(k); err == nil || ent != nil {
+            return ent.Elems()
+        }
+    }
+    return []Entry{}
+}
