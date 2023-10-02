68975b64310829415317bca30dc55d40052bd778
Enrico Weigelt, metux IT consult <info@metux.net>
2023-10-02 15:42:32 +0200

    core: document Dict
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- core/dict.go
+++ core/dict.go
@@ -6,7 +6,8 @@ import (
     "gopkg.in/yaml.v3"
 )
 
-// implemements the Entry interface
+// Simple Dictionary, based on [github.com/metux/go-magicdict/api.AnyMap],
+// implementing the [github.com/metux/go-magicdict/api.Entry] interface
 type Dict struct {
     data    * api.AnyMap
 }
@@ -25,6 +26,7 @@ func (d Dict) initMap() {
     }
 }
 
+// Get an (sub-)entry by key path
 func (d Dict) Get(k api.Key) (api.Entry, error) {
     if k.Empty() {
         return d, nil
@@ -52,6 +54,7 @@ func (d Dict) Get(k api.Key) (api.Entry, error) {
     return sub.Get(tail)
 }
 
+// Return the direct sub-keys as string slice
 func (d Dict) Keys() []string {
     d.initMap()
 
@@ -64,6 +67,7 @@ func (d Dict) Keys() []string {
     return keys
 }
 
+// Return the direct sub-elements as slice of [github.com/metux/go-magicdict/api.Entry]
 func (d Dict) Elems() []api.Entry {
     d.initMap()
 
@@ -81,6 +85,18 @@ func (d Dict) Elems() []api.Entry {
     return vals
 }
 
+// Put in an (sub-)entry by given key path. If the path has more than one element,
+// automatically diving into (and possibly creating) sub entries.
+//
+// When auto-creating and some key element has an "[]" suffix, a
+// [github.com/metux/go-magicdict/core.List] is created instead of
+// [github.com/metux/go-magicdict/core.Dict]
+//
+// Put()'ing a nil value causes that entry to be deleted from the dict.
+//
+// Hint: if the entry is scalar and constant, directly storing it's string
+// representation instead of the entry itself. But this behaviour might
+// change in future.
 func (d Dict) Put(k api.Key, v api.Entry) error {
     if k.Empty() {
         return api.ErrKeyEmpty
@@ -157,14 +173,19 @@ func (d Dict) Put(k api.Key, v api.Entry) error {
     return nil
 }
 
+// Check whether the dict is empty
 func (d Dict) Empty() bool {
     return len(*d.data) == 0
 }
 
+// Does nothing, just return "". Dicts don't have a valid string representation.
 func (d Dict) String() string {
     return ""
 }
 
+// Create a new dict from existing [github.com/metux/go-magicdict/api.AnyMap].
+// Using a *pointer* to the AnyMap, instead of copy, thus any changes in the
+// dict will be reflected in the passed AnyMap.
 func NewDict(val *api.AnyMap) Dict {
     if val == nil {
         m := make(api.AnyMap)
@@ -173,14 +194,18 @@ func NewDict(val *api.AnyMap) Dict {
     return Dict { data: val }
 }
 
+// Tell [github.com/metux/go-magicdict/magic.MagicDict] that it's allowed to
+// merge our keys with those of the lower default dict layer
 func (d Dict) MayMergeDefaults() bool {
     return true
 }
 
+// Dict objects aren't scalar at all
 func (d Dict) IsScalar() bool {
     return false
 }
 
+// Dict objects aren't constant
 func (d Dict) IsConst() bool {
     return false
 }
